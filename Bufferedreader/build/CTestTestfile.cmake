e4759d7de3e0d849014b44dbb80fc3656937cc5b
ahiralesc <adan.hirales@cetys.mx>
2023-03-05 03:55:16 +0000

    rm object files

--- Bufferedreader/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahiralesc/mysrc/Hash/Bufferedreader
-# Build directory: /home/ahiralesc/mysrc/Hash/Bufferedreader/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(bufferedReaderTest "/home/ahiralesc/mysrc/Hash/Bufferedreader/build/testBin/bufferedReaderTest")
-set_tests_properties(bufferedReaderTest PROPERTIES  WORKING_DIRECTORY "/home/ahiralesc/mysrc/Hash/Bufferedreader/build/testBin")
