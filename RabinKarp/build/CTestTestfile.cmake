8bb8dd37064808b520308327abb0bb85eec49e93
ahiralesc <adan.hirales@cetys.mx>
2023-03-12 03:40:17 +0000

    Deleted sub-proyect

--- RabinKarp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahiralesc/mysrc/Hash/Rabin_Karp
-# Build directory: /home/ahiralesc/mysrc/Hash/Rabin_Karp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
