2f10cf73284b672140469d5c57680a1711ecbf84
ahiralesc <adan.hirales@cetys.mx>
2023-03-31 22:34:50 +0000

    Fix compilation bugs

--- lsh/include/lsh.hpp
+++ lsh/include/lsh.hpp
@@ -13,17 +13,16 @@ class LSH
     /* The output stream name (path/file_name) */
     std::string osn; 
 
-		/* Shingling size. The default size is 9 words */
-		int shingling;
+	/* Shingling size. The default size is 9 words */
+	int shingling;
 
     public :
 
     /* class constructor */
-    Locality_Sensity_Hashing( 
-		    std::string _isn, std::string _osn, int _shingling ) : 
-				isn  { _isn }, 
-				osn  { _osn }, 
-				shingling { _shingling } { };
+    LSH( std::string _isn, std::string _osn, int _shingling ) : 
+		isn  { _isn }, 
+		osn  { _osn }, 
+		shingling { _shingling } { };
 
     /* Does the translation process */
     void process( );
--- lsh/src/lsh.cpp
+++ lsh/src/lsh.cpp
@@ -44,30 +44,14 @@ void parseCLA(int argc, char** argv)
 {
 	try{
 
-		TCLAP::CmdLine cmd(
-		"NAME \n\
-		\t\ lsh - Locality Sensitive Hashing \n\
-		SYNPSIS \n\
-		\t\ lsh [OPTIONS] FILE \n\
-		DESCRIPTION \n\
-		\t\ Computes a set of hash tables and stores them in a JSON ontolgy of a given text file. \n\
-		\n\
-		\t\ -f \t\ The target JSON file. \n\
-		\n\
-		\t\ -i \t\ The input text file. \n\
-		\n\
-		\t\ -s \t\ The word shingling length. 9 by default. \n\
-		\n\
-		EXAMPLES \n\
-		\t\ zcat file.gz | lsh -osn file.json \n\
-		\t\ lsh -isn file.txt -osn file.json", ' ', "0.1");
+		TCLAP::CmdLine cmd("NAME \n \t lsh - Locality Sensitive Hashing \n SYNPSIS \n \t lsh [OPTIONS] FILE \n DESCRIPTION \n \t Computes a set of hash tables and stores them in a JSON ontolgy of a given text file. \n \n \t -f \t The target JSON file. \n \n \t -i \t The input text file. \n \n \t -s \t The word shingling length. 9 by default. \n \n EXAMPLES \n \t zcat file.gz | lsh -osn file.json \n \t lsh -isn file.txt -osn file.json", ' ', "0.1");
  
  	
 		// List of value arguments
-		TCLAP::ValueArg<string> ofn("f", "file", "the target JSON file", false, "", "string");
+		TCLAP::ValueArg<std::string> ofn("f", "file", "the target JSON file", false, "", "string");
 		cmd.add( ofn );
 
-		TCLAP::ValueArg<string> ifn("i", "file", "the input text file", false, "", "string");
+		TCLAP::ValueArg<std::string> ifn("i", "file", "the input text file", false, "", "string");
 		cmd.add( ifn );
 
 		TCLAP::ValueArg<int> k("s", "length", "the word shingling length", false, 9, "int");
@@ -80,7 +64,7 @@ void parseCLA(int argc, char** argv)
 		_shingling = k.getValue();
 
 	}catch(TCLAP::ArgException &e) {
-            cerr << "Error: " << e.error() << " for argument " << e.argId() << endl;
+            std::cerr << "Error: " << e.error() << " for argument " << e.argId() << std::endl;
             exit(EXIT_FAILURE);
 	}
 }
@@ -93,28 +77,12 @@ int main(int argc, char** argv)
 {
     parseCLA( argc, argv );
 
-    Hyperplane_LSH lsh{ _isn, _osn, _shingling }; 
+    LSH lsh{ _isn, _osn, _shingling }; 
     lsh.process();
     
     return 0;
 }
 
-/**
-*   Coo`ddrdinates the translation process
-*/
-void GUTTAccept::translate(string &line) {
-    TaskEvent event{line,"cvs"};
-    // Considera borar id en traza
-   auto elem = traces.find(event.job_id);
-    if( elem == traces.end() ) {
-        Trace trace{};
-        trace.insert(event);
-        traces.insert(pair<unsigned long long,Trace>(event.job_id,trace));
-    } else {
-       Trace *trace = &elem->second;
-       trace->insert(event);
-    }
-}
 
 
 /**
@@ -129,67 +97,30 @@ void LSH::process( )
 	if( !isn.empty() ) {
 		
 		// create the word buffered reader
-		std::ifstream file( isn );
-		if( !file.is_open()) {
-			std:cerr << "Could not open input file: " << file << std::endl;
-			return EXIT_FAILURE;
+		std::ifstream _file( isn );
+		if( !_file.is_open()) {
+			std::cerr << "Could not open input file: " << isn << std::endl;
+			exit(EXIT_FAILURE);
 		}
 
 		// load the file word strings
-		while(file >> word) 
+		while(_file >> word) 
 			buffer.push_back(word);
 
 		// close the file
-		file.close();
+		_file.close();
 	} else {
         // Parse the input by processing the standard input
-        while(cin) {
-            getline( cin, input_line );
+        while(std::cin) {
+            std::getline( std::cin, input_line );
             
             if( !input_line.empty() ) {
-				istringstream ss(str);
+				std::istringstream ss(input_line);
 
-                translate( input_line );
 			}
         }
 
         fflush( stdout );
     }
 
-
-    ofstream afos( aos, std::ofstream::trunc );
-    ofstream rfos( ros, std::ofstream::trunc );
-
-    if( !afos || !rfos ) {
-        cerr << "Error: could not open file for output" << endl;
-        exit(EXIT_FAILURE);
-    }
-    bool fta = true, ftr = true;
-
-    afos << "{\"traces\":[";
-    rfos << "{\"traces\":[";
-
-    // Generating output to files 
-    for( map<unsigned long long,ETrace::Trace>::iterator it = traces.begin(); it != traces.end(); ++it ) {
-        if((*it).second.isComplete()) {
-            if( fta ) {
-                afos << (*it).second.to_json();
-                fta = false;
-            } else 
-                afos << "," << std::endl << (*it).second.to_json();
-        }else{
-            if( ftr ) {
-                rfos << (*it).second.to_json();
-                ftr = false;
-            } else 
-                rfos << "," << std::endl << (*it).second.to_json();
-        }
-       // n++;
-    }
-
-    afos << "]}";
-    rfos << "]}";
-    
-    afos.close();
-    rfos.close();
 }
