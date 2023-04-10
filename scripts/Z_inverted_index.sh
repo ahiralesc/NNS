fab0bfd6a61c53100c708df4817dc1dfb9e5771e
ahiralesc <adan.hirales@cetys.mx>
2023-04-10 02:08:15 +0000

    Added bash inverted index

---
+++ scripts/Z_inverted_index.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumenParser(description='Creates an inverted index of tokens given a set of TXT files stored in a directory tree')
+parser.add_argument('string', metavar='t', type=string, nargs=1,
+	help='a character used for tokenizing a text stream')
+parser.add_argument('string', metavar='o', type=string, nargs=1,
+	help='the destination file')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+print("Hola")
