4b3003f683c61552f19c46cea1fe476ae69006a9
ahiralesc <adan.hirales@cetys.mx>
2023-04-10 02:07:38 +0000

    Deleted python inverted index

--- scripts/inverted_index.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumenParser(description='Creates an inverted index of tokens given a set of TXT files stored in a directory tree')
-parser.add_argument('string', metavar='t', type=string, nargs=1,
-	help='a character used for tokenizing a text stream')
-parser.add_argument('string', metavar='o', type=string, nargs=1,
-	help='the destination file')
-
