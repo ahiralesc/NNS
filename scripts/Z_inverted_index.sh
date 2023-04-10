ca69402509edd0782c5d880ae9d9e175720deb30
ahiralesc <adan.hirales@cetys.mx>
2023-04-10 21:46:22 +0000

    Completed the inverted index

--- scripts/Z_inverted_index.sh
+++ scripts/Z_inverted_index.sh
@@ -1,13 +1,37 @@
-#!/usr/bin/python3
+#!/bin/bash
 
-import argparse
+# Copyright Hash-LSH Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-parser = argparse.ArgumenParser(description='Creates an inverted index of tokens given a set of TXT files stored in a directory tree')
-parser.add_argument('string', metavar='t', type=string, nargs=1,
-	help='a character used for tokenizing a text stream')
-parser.add_argument('string', metavar='o', type=string, nargs=1,
-	help='the destination file')
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-print("Hola")
+###########################################################################
+#
+# Author: Adan Hirales Carbajal
+# Email : adan.hirales@cetys.mx
+#
+###########################################################################
+
+# This script is very simple. I user awk to assign a unique integer (Z) label
+# to a unique string stores stored in lexicographical order. The script syntax
+# is the following:
+#
+# ./Z_inverted_index.sh source_dictionary inverted_index_file
+#
+#
+
+# Evaluate if arguments are given
+[ $# < 3 ] && {echo "Usage: $0 source_dictionary target_dictionary"; exit 1; }
+
+# Assign integer labels to the strings in the dictionary
+cat $1 | awk 'BEGIN { count=0 } { count++; print $0 "," count }' > $2
