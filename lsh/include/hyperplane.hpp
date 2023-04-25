707e3a29bb352db84e4aaa9aa0b052083c08149d
ahiralesc <adan.hirales@cetys.mx>
2023-04-25 00:04:31 +0000

    LSH orthogonal

---
+++ lsh/include/hyperplane.hpp
@@ -0,0 +1,19 @@
+#ifndef __HYPERPLANE_LSH__
+#define __HyPERPLANE_LSH__
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Hyperplane : public LSH {
+
+// Number of hyperplanes
+int nhyperplanes;
+
+// Buckets
+std::unordered_map<std::string, std::vector<int>> hash_map;
+
+
+};
+
+#endif
