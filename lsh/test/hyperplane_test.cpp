42e9f6cf0996b20b2f6ed0f61063bb6a2749c534
ahiralesc <adan.hirales@cetys.mx>
2023-05-02 09:07:31 -0700

    Hyperplane test

---
+++ lsh/test/hyperplane_test.cpp
@@ -0,0 +1,47 @@
+#define BOOST_TEST_MODULE "Hyperplane test"
+#define BOOST_TEST_MAIN 
+
+#include <boost/test/unit_test.hpp>
+#include <bitset>
+#include <lsh.hpp>
+#include <hyperplane.hpp>
+#include <Eigen/Dense>
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(HyperplaneTest)
+
+BOOST_AUTO_TEST_CASE(Hyperplane_test)
+{
+	int k = 9;
+	bitset<3> state;
+	LSH lsh{"../../../data/shingle_test.txt", " ", k};
+ 	lsh.load_text();
+
+	state.reset();
+
+	Eigen::VectorXf v = lsh.get_shingle();
+	Eigen::VectorXf u(k);
+	u << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+	
+	if (u == v)
+		state.set(0);
+	
+	v = lsh.get_shingle();
+	u << 2, 3, 4, 5, 6, 7, 8, 9, 10;
+
+	if (u ==v)
+		state.set(1);
+	
+	v = lsh.get_shingle();
+	u << 3, 4, 5, 6, 7, 8, 9, 10, 0;
+	
+	if (u == v)
+		state.set(2);
+
+	std::cout << v;
+
+	BOOST_CHECK_EQUAL(state.all(), true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
