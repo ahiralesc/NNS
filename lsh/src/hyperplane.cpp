318faba511ce77f1e815da68213a01bed9343c71
ahiralesc <adan.hirales@cetys.mx>
2023-05-08 19:02:48 -0700

    Constants

--- lsh/src/hyperplane.cpp
+++ lsh/src/hyperplane.cpp
@@ -1,16 +1,23 @@
 #include <cmath>
 #include <Eigen/Dense>
-#include <EigenRand/EigenRand>
 #include <iostream>
 #include "hyperplane.hpp"
 
 void Hyperplane::preprocess()
 {
-	double p = ceil( buffer.size()/k);
-	double h = log(log(p));
+	 /* let n be the total number of points (shingles) */
+    unsigned int n = ceil( buffer.size()/k );
 
-	Rand::Vmt19937_64 urng{ 42 };
-	Eigen::MatrixXf mat = Rand::balanced<Eigen::MatrixXf>(k,h,urng);
+	/* Let k = log_{1/P2}(n), thus k = log_{10}(n) / log_{10}(1/P2) */
+	unsigned int k = ceil( log10(n) / log10(1/P2) );
+	
+	/* Let p = ln(1/P1) / ln(1/P2) */
+	float p = log(1/P1) / log(1/P2);
+
+	/* Let L be the total number of buckets, with L = n^{p} */
+	unsigned int L = ceil( pow(n, p) );
+
+    // Eigen::MatrixXf N = Eigen::Matrix
 
 }
 
